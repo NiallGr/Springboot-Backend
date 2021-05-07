e383946a4f700468e15674e7f139d18b12c2a7e2
Niall Grainger <niallgrainger@gmail.com>
2021-05-06 20:53:04 -0700

    Model created + Getters and setters generated

---
+++ src/main/java/StoreBackend/Springboot/model/FoodItem.java
@@ -0,0 +1,64 @@
+package StoreBackend.Springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="foodItems")
+public class FoodItem {
+	
+			@Id
+			@GeneratedValue(strategy = GenerationType.IDENTITY)
+			private long id;
+			
+			@Column(name = "food_name")
+			private String Name;
+			
+			@Column(name = "food_group")
+			private String FoodGroup; 
+			
+			@Column(name = "food_price")
+			private float Price;
+			
+			public FoodItem() {
+				
+			}
+		
+			public FoodItem(String name, String foodGroup, float price) {
+				super();
+				Name = name;
+				FoodGroup = foodGroup;
+				Price = price;
+			}
+			
+			public long getId() {
+				return id;
+			}
+			public void setId(long id) {
+				this.id = id;
+			}
+			public String getName() {
+				return Name;
+			}
+			public void setName(String name) {
+				Name = name;
+			}
+			public String getFoodGroup() {
+				return FoodGroup;
+			}
+			public void setFoodGroup(String foodGroup) {
+				FoodGroup = foodGroup;
+			}
+			public float getPrice() {
+				return Price;
+			}
+			public void setPrice(float price) {
+				Price = price;
+			}
+			
+			
+}
