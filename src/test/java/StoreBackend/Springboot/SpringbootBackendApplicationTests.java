117b61d14f0ad5f174921f7ed1fa4cf52323f462
Niall Grainger <niallgrainger@gmail.com>
2021-05-06 20:21:04 -0700

    First commit

---
+++ src/test/java/StoreBackend/Springboot/SpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package StoreBackend.Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
