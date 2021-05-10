a872e145cc6ede2c9b489373394e8049b1e15df6
Niall Grainger <niallgrainger@gmail.com>
2021-05-09 20:40:47 -0700

    Created a Post request for adding FoodItem

--- src/main/java/StoreBackend/Springboot/controller/foodItemController.java
+++ src/main/java/StoreBackend/Springboot/controller/foodItemController.java
@@ -3,14 +3,19 @@ package StoreBackend.Springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import StoreBackend.Springboot.model.FoodItem;
 import StoreBackend.Springboot.repository.foodItemRepository;
 
+// connect between Angular and Spring
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1/")
 public class foodItemController {
 
@@ -23,4 +28,12 @@ public class foodItemController {
 	public List<FoodItem> getAllFoodItem() {
 		return FoodItemRepository.findAll();
 	}
+
+
+// Create FoodItem 
+	@PostMapping("/foodItems")
+	public FoodItem createFoodItem(@RequestBody FoodItem fooditem) {
+		return FoodItemRepository.save(fooditem);
+	}
+	
 }
